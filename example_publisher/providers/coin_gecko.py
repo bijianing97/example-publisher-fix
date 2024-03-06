a49cd1ee466dab12a36e419e583ba3c5c499a495
bijianing97 <826015751@qq.com>
2024-03-06 11:54:11 +0800

    Add eth/btc symbol

--- example_publisher/providers/coin_gecko.py
+++ example_publisher/providers/coin_gecko.py
@@ -12,7 +12,8 @@ Id = str  # The "API id" of the CoinGecko price, listed on CoinGecko page for ea
 
 USD = "usd"
 
-eth_btc_symbol = 'eth/btc'  # 特殊处理的产品标识
+eth_btc_symbol = 'USDB/USD'
+eth_btc_id = 'eth/btc'  # 特殊处理的产品标识
 ethereum_id = 'ethereum'  # 以太坊的ID，用于获取价格
 bitcoin_id = 'bitcoin'
 
@@ -32,7 +33,7 @@ class CoinGecko(Provider):
         for coin_gecko_product in self._config.products:
             if coin_gecko_product.symbol in product_symbols:
                 id = coin_gecko_product.coin_gecko_id
-                if id != eth_btc_symbol:
+                if id != eth_btc_id:
                     new_prices[id] = self._prices.get(id, None)
             else:
                 raise ValueError(
@@ -46,9 +47,9 @@ class CoinGecko(Provider):
 
             # 避免除以零
                 if bitcoin_price != 0:
-                    new_prices[eth_btc_symbol] = ethereum_price / bitcoin_price
+                    new_prices[eth_btc_id] = ethereum_price / bitcoin_price
                 else:
-                    new_prices[eth_btc_symbol] = 0  # 或者设定一个错误值/标记
+                    new_prices[eth_btc_id] = 0  # 或者设定一个错误值/标记
                     log.error("Bitcoin price is zero. Skipping 'eth/btc'.")
         else:
             # 如果没有 ethereum 或 bitcoin 的价格，可以选择记录错误或跳过
@@ -63,23 +64,23 @@ class CoinGecko(Provider):
 
     def _update_prices(self) -> None:
 
-        product_ids = [id_ for id_ in self._prices.keys() if id_ != eth_btc_symbol]
+        product_ids = [id_ for id_ in self._prices.keys() if id_ != eth_btc_id]
         result = self._api.get_price(
             ids=product_ids, vs_currencies=USD, precision=18
         )
         for id_, prices in result.items():
             self._prices[id_] = prices[USD]
 
-        if eth_btc_symbol in self._prices:
+        if eth_btc_id in self._prices:
             # 确保我们有以太坊和比特币的价格
             if ethereum_id in self._prices and bitcoin_id in self._prices:
                 ethereum_price = self._prices[ethereum_id]
                 bitcoin_price = self._prices[bitcoin_id]
 
                 if bitcoin_price != 0:
-                    self._prices[eth_btc_symbol] = ethereum_price / bitcoin_price
+                    self._prices[eth_btc_id] = ethereum_price / bitcoin_price
                 else:
-                    self._prices[eth_btc_symbol] = 0  # 或设置为一个错误值/标记
+                    self._prices[eth_btc_id] = 0  # 或设置为一个错误值/标记
                     log.error("Bitcoin price is zero. Skipping 'eth/btc'.")
             else:
                 log.error("Ethereum or Bitcoin price not found. Skipping 'eth/btc'.")
